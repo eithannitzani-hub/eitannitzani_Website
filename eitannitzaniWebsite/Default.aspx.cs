12786686e5b65608ebab5e45bff02db7a0e47c74
adamkolikant <adam.kolikantnew@gmail.com>
2025-12-31 12:05:16 +0200

    לוח הכפל

---
+++ eitannitzaniWebsite/Default.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    public string kefel;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        kefel = "<table border = '1'>";
+        for (int i = 1; i <= 10; i++)
+        {
+            kefel += " <tr>";
+            for (int j = 1; j <= 10; j++)
+            {
+                kefel += "<td> " + (i * j) + "</td>";
+            }
+            kefel += "</tr>";
+        }
+        kefel += "</table>";
+
+
+
+    }
+}
