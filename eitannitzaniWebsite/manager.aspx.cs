dd8256d3b3a81dd7914bc3997e94a7971187d12c
User <User@302-18>
2026-06-03 11:34:38 +0300

    ##

--- eitannitzaniWebsite/manager.aspx.cs
+++ eitannitzaniWebsite/manager.aspx.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -9,6 +11,19 @@ public partial class manager : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (!IsPostBack)
+        {
+            ShowUsers();
+        }
+    }
 
+    private void ShowUsers()
+    {
+        string sql = "SELECT * FROM tUsers";
+
+        DataTable dt = Helper.ExecuteDataTable("Database.mdf", sql);
+
+        gvUsers.DataSource = dt;
+        gvUsers.DataBind();
     }
 }
